565c530e70ab4069bf8fe9d7aa1d2f52b92d0549
Charmi <cparmar01@gmail.com>
2024-03-12 00:17:02 +0530

    css modified

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <h1>Welcome to Our Product Management System</h1>
     <p>Welcome to our platform for managing your products easily! From planning to launching, we've got you covered with tools to make your job simpler and more effective.</p>
     <button><Link to={'/Products'} className='edit'>
-                            Products
+                            Go To Products
                           </Link></button>
   </div>
   )
